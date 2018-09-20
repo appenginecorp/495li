b579c6a488525f32db6e1be3d27bdd40dd89352a
kirk <yezcoinmail@gmail.com>
2018-09-20 18:45:10 -0400

    move header topbar login modal to seperate file
    load some content via jquery

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  //console.log("doc ready");
+    $("#header-nav-bar").load("header.html");
+    $("#all .top-bar").load("topbar.html");
+    $("#login-modal").load("loginmodal.html");
+});
