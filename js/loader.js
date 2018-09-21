e0aba767172c9288b45a64cf095f467830e1da06
kirk <yezcoinmail@gmail.com>
2018-09-21 13:41:39 -0400

    added sale listing page

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   //console.log("doc ready");
+    $(document).attr("title", "New York Properties Investment & Management");
     $("#header-nav-bar").load("header.html");
     $("#all .top-bar").load("topbar.html");
     $("#login-modal").load("loginmodal.html");
+    $("#footer-copyright").load("ft-cpyrght.html");
 });
